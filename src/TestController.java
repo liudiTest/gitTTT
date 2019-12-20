ec0807973f014487b56f1b73a05f94cadf31b5c2
liudi <470382612@qq.com>
2019-12-20 17:12:17 +0800

    Merge branches 'master' and 'uat' of https://github.com/liudiTest/gitTTT into uat
    # Conflicts:
    #	src/TestController.java

--- src/TestController.java
+++ src/TestController.java
@@ -2,5 +2,11 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("ces111");
         System.out.println("ces2");
+        System.out.println("ces111");
+        System.out.println("ces2");
+        System.out.println("ces111");
+        System.out.println("ces2");
+        System.out.println("ces111");
+        System.out.println("ces2");
     }
 }
